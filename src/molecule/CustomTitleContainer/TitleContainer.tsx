7649a4546ed9002720f8339b261f55e1bd993085
ArjooReact <electronicsarzoo@gmail.com>
2024-12-22 18:19:03 +0530

    making box responsive

--- src/molecule/CustomTitleContainer/TitleContainer.tsx
+++ src/molecule/CustomTitleContainer/TitleContainer.tsx
@@ -4,8 +4,8 @@ import './TitleContainer.css'
 
 export const TitleContainer:React.FC<TitleContainerType>=({title})=>{
     return(<div className="title-mainContainer">
-        <p style={{fontSize:'16px',fontWeight:'400',textAlign:'center'}}>1-12 of Over 1,000 results for</p>
-        <p style={{fontSize:'16px',marginLeft:'8px',fontWeight:'bolder',color:'orange'}}>Departments</p>
+        <p className="title-first-text-size">1-12 of Over 1,000 results for</p>
+        <p className="title-second-text-size">Departments</p>
     </div>)
 }
 export default TitleContainer
